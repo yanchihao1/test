137487363ab5ab7c7f55e68266720f2c2b4b725a
yanchihao <yanchihao@shinetechchina.com>
2019-09-18 10:15:40 +0800

    addnewclass

---
+++ coreStudy/coreStudy/testclass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace coreStudy
+{
+    public class testclass
+    {
+        /// <summary>
+        /// 添加新方法测试提交
+        /// </summary>
+        public void addclass()
+        {
+
+        }
+    }
+}
